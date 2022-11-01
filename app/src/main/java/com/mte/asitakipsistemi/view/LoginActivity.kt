8f091ac7d931398ecf5bb6076f8c179d6dd0842b
Muhammet Talha Ergün <mtalhaergun24@gmail.com>
2022-11-01 18:19:52 +0300

    Proje Oluşturuldu

---
+++ app/src/main/java/com/mte/asitakipsistemi/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.mte.asitakipsistemi.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mte.asitakipsistemi.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
