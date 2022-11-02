deda98ecde4ac1e00e51581a6a130529fa14c2f3
Muhammet Talha Ergün <mtalhaergun24@gmail.com>
2022-11-02 18:47:56 +0300

    Sayfa ve Fragment Görünümleri

--- app/src/main/java/com/mte/asitakipsistemi/view/MainActivity.kt
+++ app/src/main/java/com/mte/asitakipsistemi/view/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.mte.asitakipsistemi.view
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationBarView
 import com.mte.asitakipsistemi.R
+import com.mte.asitakipsistemi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.account -> replaceFragment(AccountFragment())
+                R.id.vaccines -> replaceFragment(VaccinesFragment())
+
+                else ->{
+
+                }
+            }
+            true
+        }
     }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+
+
 }
